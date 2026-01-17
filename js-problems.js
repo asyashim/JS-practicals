01315337eb3f2cd963069babc48b4b222b7232a8
Asyabi <asyabicp@gmail.com>
2026-01-17 21:01:01 +0530

    Initial commit - JS practicals

---
+++ js-problems.js
@@ -0,0 +1,112 @@
+//Find longest word in sentence
+ function findLongestWord(string){
+   let str =string.split(" ");
+   let longest=0;
+   let longestWord="";
+   
+   for(let word of str){
+    if(word.length>longest){
+        longest=word.length;
+        longestWord=word;
+    }
+   }
+   return longestWord;
+}
+
+let str="hello goodmorning everyone";
+console.log(findLongestWord(str))
+
+function longest(string){
+    let str=string.split(" ");
+    return str.reduce((max,curr)=>max.length>curr.length?max:curr,0)
+}
+console.log(longest(str))
+
+function longestWord(string){
+    let str=string.split(" ")
+    let sort=str.sort((a,b)=>b.length-a.length)
+    return sort[0]
+}
+console.log(longestWord(str))
+
+
+//Remove vowels from the sentence
+
+function removeVowels(str){
+    let vowels="aeiou";
+    let string="";
+    for(let i=0;i<str.length;i++){
+        let isVowels=false;
+        for(let j=0;j<vowels.length;j++){
+            if(str[i]===vowels[j]){
+                isVowels=true
+                break;
+
+            }
+        }if(!isVowels){
+                string+= str[i]
+            
+        }
+    }return string;
+}
+console.log(removeVowels(str))
+
+function remove(str){
+    let vowels="aeiou";
+    let string=""
+    
+    for(let word of str){
+        if(!vowels.includes(word)){
+            string+=word
+        }
+    }return string;
+}
+
+console.log(remove(str))
+
+
+function remove1(str){
+    let string=str.split("");
+    let vowels="aeiou"
+    return string.filter(ch=>!vowels.includes(ch)).join('')
+}
+
+console.log(remove1(str))
+
+function remove3(str){
+    let string=str.split("")
+    let result=""
+    let vowels="aeiou"
+     string.forEach(ch=>{
+        if(!vowels.includes(ch)){
+            result+=ch
+        }
+    })
+    return result;
+}
+
+console.log(remove3(str))
+//Remove vowels from string
+//Missing numbers from an array
+//Palindrome
+//Print 1 to 10 using setTimeout
+//Count duplicate elements
+//Print 1 to 10 with 1 second gap 
+//Largest element from an array
+//Smallest element from an array
+//Reverse a string
+//Check if two strings are anagrams
+//Check if a number is prime
+//Factorial of a number
+//Sum and avg of elements from an array
+//Count odd and even numbers
+//Swapping
+//Add only truthy values in an array
+//generate odd numbers in between 1 and 10 in reverse order
+//Add an element to an array without .push or .unshift
+//Remove the last element of an array without .pop.
+
+//Reverse an array without using built-in methods.
+//Merge 2 arrays manually
+//Find if a given element exists in an array (no .includes)
+//First occurance of the targe
