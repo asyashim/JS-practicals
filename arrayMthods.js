e81e06470150152b2652780fe7cb885cca29aa15
Asyabi.C.P <asyabicp@gmail.com>
2026-01-30 14:36:42 +0530

    Add array methods for doubling and filtering numbers
    Practised map() and filter() methods using sample arrays.

---
+++ arrayMthods.js
@@ -0,0 +1,9 @@
+//Double number using map
+let array=[12,34,56,78]
+let double= array.map(x=>x*2);
+console.log(double)
+
+//Find even numbers using filter
+let array=[12,23,34,45,56,78]
+let even=array.filter(x=>x%2===0);
+console.log(even)
